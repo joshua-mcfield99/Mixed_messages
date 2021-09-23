a53ab2ca2c7fec2e730ce48db2c33b7d36939826
Josh Mcfield <joshua.mclelland2@yahoo.com>
2021-09-23 13:08:28 +0100

    Index selector

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ import{quotes, authors, dates} from "./object.js";
 
 
 
+const selector = (arr) =>{
+    const index = Math.floor(Math.random()*arr.length);
+    let quote = "", author = "", date="";
+    if(arr === quotes) {
+        quote = arr[index];
+        return quote;
+    } else if (arr === authors) {
+        author = arr[index];
+        return author;
+    } else if (arr === dates){
+        date = arr[index];
+        return date;
+    } else {
+        return 'Try again';
+    };
+    
+}
+
+console.log(selector(quotes));
+console.log(selector(dates));
+console.log(selector(authors));
